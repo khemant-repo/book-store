9bf5b4820aa34160a23eb58cbe0e6f7523bde4fd
Hemant Kumar <hemkumar11@publicisgroupe.net>
2025-07-10 19:29:28 +0530

    book store App with catalog service

---
+++ src/main/java/com/sivalabs/spring_boot_microservices_course/SpringBootMicroservicesCourseApplication.java
@@ -0,0 +1,13 @@
+package com.sivalabs.spring_boot_microservices_course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroservicesCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroservicesCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sivalabs/spring_boot_microservices_course/SpringBootMicroservicesCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sivalabs.spring_boot_microservices_course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicroservicesCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
